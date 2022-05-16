563ea68e2b2f2aa1d5f7bcdd5b4015c8ef5d9d4d
unknown <mairurated@gmail.com>
2022-05-16 20:01:43 +0300

    First file

---
+++ scripts/run.js
@@ -0,0 +1,40 @@
+const main = async () => {
+  const [owner, randomPerson] = await hre.ethers.getSigners();
+  const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
+  const waveContract = await waveContractFactory.deploy();
+  await waveContract.deployed();
+
+  console.log("Contract deployed to:", waveContract.address);
+  console.log("Contract deployed by:", owner.address);
+
+  let waveCount;
+  waveCount = await waveContract.getTotalWaves();
+
+  let totalWaves = [];
+
+  let waveTxn = await waveContract.wave();
+  await waveTxn.wait();
+  totalWaves.push(waveContract.address);
+
+  waveCount = await waveContract.getTotalWaves();
+
+  waveTxn = await waveContract.connect(randomPerson).wave();
+  await waveTxn.wait();
+  totalWaves.push(waveContract.address);
+
+  waveCount = await waveContract.getTotalWaves();
+  console.log("We have " + totalWaves.length + " waves in total.");
+  console.log(totalWaves);
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+runMain();
